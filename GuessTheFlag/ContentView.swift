72afe4c3331f35b170ab9c8a95c8565e63f8edf4
stimLite <gross2433@gmail.com>
2023-10-28 14:49:16 +0300

    Guess flag game

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,17 +7,116 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var newGame = false
+    @State private var countGame = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+
+                    ForEach(0..<3) { number in
+                        Button {
+                            if countGame >= 7 {
+                                startNewGame(number)
+                            } else {
+                                flagTapped(number)
+                            }
+                        } label: {
+                            Image(countries[number])
+//                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+//                .clipShape(.rect(cornerRadius: 20))
+
+                Spacer()
+                Spacer()
+
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+        .alert(scoreTitle, isPresented: $newGame) {
+            Button("New Game", action: scoreNewGame)
+        } message: {
+            Text("for continium game, plase taped New Game")
+        }
+    }
+
+    func flagTapped(_ number: Int) {
+        countGame += 1
+        if number == correctAnswer {
+            scoreTitle = "Correct, this is flag of \(countries[number])"
+            score += 1
+        } else {
+            scoreTitle = "Wrong! That’s the flag of \(countries[number])"
+        }
+        showingScore = true
+        
+    }
+    
+    func startNewGame(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+        }
+        scoreTitle = "Congratulations!!!! Your total scor: \(score)"
+        countGame = 0
+        newGame = true
+    }
+    
+    func scoreNewGame() {
+        score = 0
+        askQuestion()
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
